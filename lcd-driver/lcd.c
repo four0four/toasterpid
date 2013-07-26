7665e42f0fedee90f6a031a6410119c0537999a3
four0four <shmeeeh@gmail.com>
2013-07-26 12:27:53 -0400

    basic lcd communication code. no init yet

--- lcd-driver/lcd.c
+++ lcd-driver/lcd.c
@@ -1,1 +1,45 @@
 #include "lcd.h"
+
+
+void writeCMD(uint8_t cmd) {
+  // EN low, RS low for cmd
+  LCD_CTL_PORT &= ~((1<<LCD_E)|(1<<LCD_RS));
+  // clear data lines for high nibble
+  LCD_DATA_PORT &= ~LCD_DATA_MASK; 
+  LCD_DATA_PORT |= (cmd & 0xF0) & LCD_DATA_MASK;
+  // allow read
+  LCD_CTL_PORT |= (1<<LCD_E);
+  // need to wait ~500ns, double is overkill anyway
+  _delay_us(1);                                   
+  LCD_CTL_PORT &= ~(1<<LCD_E);
+  _delay_ms(6);
+  // clear data lines for low nibble
+  LCD_DATA_PORT &= ~LCD_DATA_MASK; 
+  LCD_DATA_PORT |= (cmd & 0x0F) & LCD_DATA_MASK;
+  _delay_us(1);                                   
+  // clear EN to finish
+  LCD_CTL_PORT &= ~(1<<LCD_E);
+  _delay_ms(6);
+}
+
+void writeData(uint8_t data) {
+  // EN low, RS high for data
+  LCD_CTL_PORT &= ~(1<<LCD_E);
+  LCD_CTL_PORT |= (1<<LCD_RS);
+  // clear data lines for high nibble
+  LCD_DATA_PORT &= ~LCD_DATA_MASK; 
+  LCD_DATA_PORT |= (data & 0xF0) & LCD_DATA_MASK;
+  // allow read
+  LCD_CTL_PORT |= (1<<LCD_E);
+  // need to wait ~500ns, double is overkill anyway
+  _delay_us(1);                                   
+  LCD_CTL_PORT &= ~(1<<LCD_E);
+  _delay_us(500);
+  // clear data lines for low nibble
+  LCD_DATA_PORT &= ~LCD_DATA_MASK; 
+  LCD_DATA_PORT |= (data & 0x0F) & LCD_DATA_MASK;
+  _delay_us(1);                                   
+  // clear EN to finish
+  LCD_CTL_PORT &= ~(1<<LCD_E);
+  _delay_us(500);
+}
--- lcd-driver/lcd.h
+++ lcd-driver/lcd.h
@@ -2,15 +2,49 @@
 #define __LCD_H
 
 #include <avr/io.h>
+#include <util/delay.h>
 #include <stdint.h>
 
+// What lcd.h provides: 
+// writeData() and writeCMD() - basic communication functonality
+// writeString() and writeChar() - abstracted write functionality
+// Macros for relevant opcodes
+// Configuration to fit hardware
 
+// What lcd.h does not provide: initialization functions
+
+// All delays are provided using simple timing loops from delay.h - nothing fancy
+
+// control lines
 #define LCD_CTL_PORT PORTD
 #define LCD_E
 #define LCD_RS
 
-// the following assumes the use of the lower nibble
+// data lines/bus
 #define LCD_DATA_PORT PORTB
+#define LCD_DATA_MASK 0x0F
+
+// basic LCD commands. for commands with following options, simply bitwise OR the command w/the base
+#define LCD_CLR 0x01 // clear the LCD entirely, cursor pos = 0
+#define LCD_HOME 0x02 // return cursor position, reset shifting position
+// - LCD entry mode control
+#define LCD_ENTRY 0x04 // entry mode base
+#define ENTRY_INCREMENT 0x02 // set to increment cursor during write
+#define ENTRY_SHIFT 0x01 // set to shift display
+// - LCD power control
+#define LCD_DISPLAY 0x08 // power control base
+#define DISPLAY_ON 0x04 // set to power on, clear to power off
+#define DISPLAY_CURSOR 0x02 // set to solid cursor, clear for no cursor (check)
+#define DISPLAY_BLINK_CURSOR 0x01 // set to blink LCD cursor (check as well)
+// - LCD cursor control
+#define LCD_CURSOR 0x10 // control direction/behavior of cursor
+#define CURSOR_SHIFT 0x08 // set to shift display (default/unset is move)
+#define CURSOR_SHIFT_RIGHT 0x04 // set to shift to the right while writing (default/unset is left)
+// - LCD function control
+#define LCD_FUNCTION 0x20 // misc configuration
+#define FUNCTION_DATA_WIDTH 0x10 // set to indicate 8 bit interface, unset/zero for 4 bit
+#define FUNCTION_DISPLAY_LINES 0x08 // set to indicate 2 lines, unset for 1
+#define FUNCTION_FONT 0x04 // set to indicate 5*10 font, unset for 5*8
 
 
 void writeCMD(uint8_t);
