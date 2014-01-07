a45099d43fcd90f52bf1e30a750fdbcd15a1dd62
four0four <shmeeeh@gmail.com>
2014-01-07 16:53:44 -0500

    cleaned up lcd test

--- lcd-driver/main.c
+++ lcd-driver/main.c
@@ -1,18 +1,6 @@
-#define F_CPU 1000000
-
 #include "lcd.h"
 #include <stdint.h>
 #include <avr/io.h>
-
-
-void waitLCD() {
-  DDRB = 0x00;
-  PORTB |= (1<<LCD_RW);
-  PORTB &= ~(1<<LCD_RS);
-  while(PINB & (1<<3));
-  DDRB = 0xFF;
-  PORTB &= ~(1<<LCD_RW);
-}
 
 int main() {
 
@@ -22,33 +10,13 @@ int main() {
   PORTB = 0x00;
   PORTD = 0x00;
 
+  _delay_ms(150);
 
-  _delay_ms(110);
-  lcdWrite(0, 0x3);
-  _delay_ms(5);
-  lcdWrite(0, 0x3);
-  _delay_ms(5);
-  lcdWrite(0, 0x3);
-  _delay_ms(5);
-  lcdWrite(0, 0x2);
-  _delay_ms(3);
-
-  lcdWrite(0,0x28);
-  lcdWait();
-  lcdWrite(0,0xC);
-  lcdWait();
-  lcdWrite(0,1);
-  lcdWait();
-  lcdWrite(0,0x06);
-  lcdWait();
-
+  lcdInit();
 
   //_delay_ms(1);
 
-  while(1){
-    lcdWrite(1,'!');
-    lcdWait();
-  }
+  lcdWriteString("Hello, World!\n");
 
   return 0;
 }
